7f862cf04927c2c212d2ee4bca60b66f81f7be18
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-03-28 18:38:43 -0600

    Create simple test to please the CI gods.

--- test/test.js
+++ test/test.js
@@ -1,1 +1,23 @@
 'use strict'
+
+var request = require('request')
+  , assert = require('assert')
+
+var url = 'https://raw.githubusercontent.com/poteto/emberconf-2015/master/README.md'
+
+describe('EmberConf url', function () {
+
+  it('should not have request errors', function (done) {
+    request(url, function (err) {
+      assert.ifError(err)
+      done()
+    })
+  })
+
+  it('should exist', function (done) {
+    request(url, function (err, res) {
+      assert.equal(res.statusCode, 200)
+      done()
+    })
+  })
+})
